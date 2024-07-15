f55f1d0e95254b86ebc958bce2399f562cf2cae5
ZH <1300450091@qq.com>
2024-07-15 22:47:37 +0800

    fix: 基础样式

--- src/page/lowCodeMain.vue
+++ src/page/lowCodeMain.vue
@@ -11,9 +11,12 @@
     <div class="showcodeComponent">
       <VueDraggable v-model="list2" :animation="150" group="people" class="rightBox">
         <div v-for="item in list2" :key="item.id" class="showBox">
-          <IconSort class="handle cursor-move"></IconSort>
-          {{ item.name }}
-          <iconClose class="cursor-pointer" @click="remove(index)"></iconClose>
+          <div class="text">
+            {{ item.name }}
+          </div>
+          <div class="closePanenl">
+            关闭
+          </div>
         </div>
       </VueDraggable>
     </div>
@@ -22,7 +25,7 @@
 </template>
 
 <script setup>
-import { ref,watchEffect } from 'vue'
+import { ref, watchEffect } from 'vue'
 import { VueDraggable } from 'vue-draggable-plus'
 
 
@@ -55,13 +58,13 @@ function onClone() {
   console.log('===', list2.value)
   console.log('clone')
 }
-function remove(index) {
-  list2.value.splice(index, 1)
-}
+// function remove(index) {
+//   list2.value.splice(index, 1)
+// }
 
 watchEffect(() => {
-      console.log( list2.value);
-    });
+  console.log(list2.value);
+});
 
 </script>
 
@@ -71,22 +74,73 @@ watchEffect(() => {
   height: 100%;
   display: flex;
   flex-direction: row;
+  overflow-y: hidden;
 }
 
 .componentStack {
   width: 20vw;
   height: 100%;
+  padding: 10px;
+  overflow-y: auto;
 }
 
 .showcodeComponent {
   width: 60vw;
   background-color: rgb(237, 239, 243);
   height: 100%;
+  overflow-y: auto;
+  overflow-x: hidden;
+
 }
 
 .configurationArea {
   width: 20vw;
   /* background-color: aqua; */
   height: 100%;
+}
+
+.leftBox {
+  /* height: 100%; */
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  flex-wrap: wrap;
+}
+
+.rightBox {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.chooseBox {
+  height: 20px;
+  width: 30%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid blue;
+  cursor: move;
+}
+
+.showBox {
+  height: 100px;
+  width: 90%;
+  border: 1px solid red;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.text {
+  width: 90%;
+}
+
+.closePanenl {
+  width: 8%;
 }
 </style>
