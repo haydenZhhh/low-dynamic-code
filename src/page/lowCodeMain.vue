f7068ecbc85e73dde911d4f1383cfb69949522fc
ZH <1300450091@qq.com>
2024-07-17 23:09:30 +0800

    fix: 优化

--- src/page/lowCodeMain.vue
+++ src/page/lowCodeMain.vue
@@ -2,16 +2,8 @@
   <div class="codeMain">
     <div class="componentStack">
       <div class="title">基础组件</div>
-      <VueDraggable
-        v-model="list1"
-        :animation="150"
-        :group="{ name: 'people', pull: 'clone', put: false }"
-        :sort="false"
-        dragClass="dragClass"
-        class="leftBox"
-        @clone="onClone"
-        @end="endEnvent"
-      >
+      <VueDraggable v-model="list1" :animation="150" :group="{ name: 'people', pull: 'clone', put: false }"
+        :sort="false" dragClass="dragClass" class="leftBox " @clone="onClone" @end="endEnvent">
         <div v-for="item in list1" :key="item.id" class="chooseBox">
           {{ item.name }}
         </div>
@@ -19,39 +11,33 @@
       <br />
       <br />
       <div class="title">高级组件</div>
-      <VueDraggable
-        v-model="list3"
-        :animation="150"
-        :group="{ name: 'people', pull: 'clone', put: false }"
-        :sort="false"
-        dragClass="dragClass"
-        class="leftBox"
-        @clone="onClone"
-        @end="endEnvent"
-      >
+      <VueDraggable v-model="list3" :animation="150" :group="{ name: 'people', pull: 'clone', put: false }"
+        :sort="false" dragClass="dragClass" class="leftBox " @clone="onClone" @end="endEnvent">
         <div v-for="item in list3" :key="item.id" class="chooseBox">
           {{ item.name }}
         </div>
       </VueDraggable>
     </div>
     <div class="showcodeComponent">
-      <VueDraggable
-        v-model="list2"
-        :animation="150"
-        group="people"
-        class="rightBox"
-        ghostClass="ghost"
-      >
-        <div
-          v-for="item in list2"
-          :key="item.id"
-          :class="{ chooseKey: item.id === chooseId, showBox: true }"
-          @click="chooseTab(item)"
-        >
+      <VueDraggable v-model="list2" :animation="150" group="people" @onAdd="afterAdd" class="rightBox "
+        ghostClass="ghost">
+        <div v-for="item in list2" :key="item.id" :class="{ chooseKey: item.id === chooseId, showBox: true }"
+          @click="chooseTab(item)">
           <div class="renderPanenl">
             {{ item.name }}
           </div>
-          <div class="closePanenl">66</div>
+          <div v-if="item.id === chooseId" class="closePanenl ">
+
+            <el-popconfirm confirm-button-text="是" cancel-button-text="否" @confirm="confirmEvent(item)" title="确认删除?">
+              <template #reference>
+                <el-icon>
+                  <Delete style="height: 20px;width: 20px;color: rgb(251,83,86)" />
+                </el-icon>
+              </template>
+            </el-popconfirm>
+
+
+          </div>
         </div>
       </VueDraggable>
     </div>
@@ -62,6 +48,8 @@
 <script setup>
 import { ref, watchEffect } from 'vue';
 import { VueDraggable } from 'vue-draggable-plus';
+import { Delete } from '@element-plus/icons-vue'
+import { ElPopconfirm } from 'element-plus';
 
 const chooseId = ref('');
 
@@ -87,44 +75,51 @@ const list1 = ref([
 const list3 = ref([
   {
     name: '看看',
-    id: '1',
+    id: '8',
   },
   {
     name: '方法',
-    id: '2',
+    id: '9',
   },
   {
     name: '打点',
-    id: '3',
+    id: '12',
   },
   {
     name: '嗯嗯',
-    id: '4',
+    id: '15',
   },
 ]);
 
-const list2 = ref(
-  list1.value.map((item) => ({
-    name: `${item.name}-2`,
-    id: `${item.id}-2`,
-  }))
+const list2 = ref([]
+  // list1.value.map((item) => ({
+  //   name: `${item.name}-2`,
+  //   id: `${item.id}-2`,
+  // }))
 );
+const afterAdd = (val) => {
+  console.log('hhhh', val, list2.value)
+}
 
 const chooseTab = (val) => {
   chooseId.value = val.id;
 };
 
-const endEnvent = (val) => {
-  console.log('ppppp44', val, list2.value);
+const endEnvent = () => {
+
 };
 
-function onClone() {
-  console.log('===', list2.value);
-  console.log('clone');
+const confirmEvent = (val) => {
+  console.log('===', val)
+  list2.value = list2.value.filter((item) => item.id !== val.id)
+  chooseId.value = ''
 }
-// function remove(index) {
-//   list2.value.splice(index, 1)
-// }
+
+const onClone = (val) => {
+  console.log('===', list2.value);
+  console.log('clone', val);
+}
+
 
 watchEffect(() => {
   console.log(list2.value);
@@ -167,12 +162,13 @@ watchEffect(() => {
   /* height: 100%; */
   width: 100%;
   display: flex;
-  justify-content: center;
+  justify-content: start;
   flex-direction: row;
   margin-top: 20px;
   gap: 10px;
   flex-wrap: wrap;
 }
+
 .basicComponents {
   display: flex;
   flex-direction: row;
@@ -190,8 +186,8 @@ watchEffect(() => {
 }
 
 .chooseBox {
-  height: 20px;
-  width: 40%;
+  height: 18px;
+  width: 25%;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -210,17 +206,21 @@ watchEffect(() => {
   margin-bottom: 10px;
   cursor: move;
 }
+
 .chooseKey {
   background-color: rgb(175, 223, 245);
 }
+
 .renderPanenl {
   width: 100%;
   height: 100%;
   padding: 10px;
 }
+
 .title {
   font-weight: bold;
 }
+
 .showComponents {
   width: 100%;
   display: flex;
@@ -230,20 +230,29 @@ watchEffect(() => {
   gap: 10px;
   flex-wrap: wrap;
 }
+
 .dragClass {
   background-color: red;
 }
+
 .ghost {
   opacity: 0.5;
   background: #c8ebfb;
 }
+
 .closePanenl {
   position: relative;
   right: 20px;
   bottom: -45px;
   z-index: 999;
-  height: 20px;
-  width: 40px;
-  background-color: green;
+  height: 34px;
+  width: 34px;
+  background-color: #fff;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  border: 1px solid rgb(251, 83, 86);
 }
 </style>
